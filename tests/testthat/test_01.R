d24b6899184616b445dd591af56a823e99fab8cd
Hannes Muehleisen <hannes@muehleisen.org>
2018-01-17 17:27:16 +0100

    tests merged

--- tests/testthat/test_01.R
+++ tests/testthat/test_01.R
@@ -2,8 +2,9 @@ library(testthat)
 library(dplyr)
 library(tpchr)
 
+tbls <- dbgen(1)
+
 test_that( "dplyr backed by data frames works" , {
-	tbls <- dbgen(1)
 	s <- src_df(env = list2env(tbls))
 
 	expect_true(test_dplyr(s, 1))
@@ -16,4 +17,17 @@ test_that( "dplyr backed by data frames works" , {
 	expect_true(test_dplyr(s, 8))
 #	expect_true(test_dplyr(s, 9))
 	expect_true(test_dplyr(s, 10))
+})
+
+
+test_that( "MonetDBLite works" , {
+	c <- dbConnect(MonetDBLite::MonetDBLite(), ":memory")
+	lapply(names(tbls), function(n) {dbWriteTable(c, n, tbls[[n]])})
+	lapply(1:22, function(n) {expect_true(test_dbi(c, n))})
+})
+
+test_that( "SQLite works" , {
+	c <- dbConnect(RSQLite::SQLite())
+	lapply(names(tbls), function(n) {dbWriteTable(c, n, tbls[[n]])})
+	lapply(1:22, function(n) {expect_true(test_dbi(c, n))})
 })
