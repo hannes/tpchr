c5e3070ca574b42d94b6aa09797c71a48e0ccef1
Hannes Muehleisen <hannes@muehleisen.org>
2018-01-18 11:45:28 +0100

    added fuzzy comparision

--- tests/testthat/test_01.R
+++ tests/testthat/test_01.R
@@ -4,17 +4,15 @@ library(tpchr)
 library(DBI)
 
 tbls <- dbgen(1)
-q <- c(1,2,4,5,6,7,8,10) # 3 and 9 have issues, 11++ needs work
 
 test_that( "dplyr backed by data frames" , {
 	s <- src_df(env = list2env(tbls))
-	lapply(q, function(n) {expect_true(test_dplyr(s, n))})
+	lapply(1:10, function(n) {expect_true(test_dplyr(s, n))})
 })
-
 
 test_that( "MonetDBLite" , {
 	c <- dbConnect(MonetDBLite::MonetDBLite())
 	lapply(names(tbls), function(n) {dbWriteTable(c, n, tbls[[n]])})
-	lapply(q, function(n) {expect_true(test_dbi(c, n))})
+	lapply(c(1:13, 16:22), function(n) {expect_true(test_dbi(c, n))})
 	dbDisconnect(c, shutdown=T)
 })
