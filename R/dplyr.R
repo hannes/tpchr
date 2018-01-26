05e2c677b56140e078bc4d6353f7fe865d83b4a0
Hannes Muehleisen <hannes@muehleisen.org>
2018-01-26 11:30:37 +0100

    more opt

--- R/dplyr.R
+++ R/dplyr.R
@@ -2,18 +2,22 @@ library(dplyr)
 
 test_dplyr_q <- list()
 
+# optimized
 test_dplyr_q[[1]] <- function(s) {
     tbl(s, "lineitem") %>% 
+        select(l_shipdate, l_returnflag, l_linestatus, l_quantity, l_extendedprice, l_discount, l_tax) %>%
         filter(l_shipdate <= "1998-09-02") %>% 
+        select(l_returnflag, l_linestatus, l_quantity, l_extendedprice, l_discount, l_tax) %>%
         group_by(l_returnflag, l_linestatus) %>% 
         summarise(
-            sum_qty=sum(l_quantity), 
-            sum_base_price=sum(l_extendedprice), 
-            sum_disc_price=sum(l_extendedprice * (1 - l_discount)), 
-            sum_charge=sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)), 
-            avg_qty=mean(l_quantity), 
-            avg_price=mean(l_extendedprice), 
-            avg_disc=mean(l_discount), count_order=n()) %>% 
+            sum_qty        = sum(l_quantity), 
+            sum_base_price = sum(l_extendedprice), 
+            sum_disc_price = sum(l_extendedprice * (1 - l_discount)), 
+            sum_charge     = sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)), 
+            avg_qty        = mean(l_quantity), 
+            avg_price      = mean(l_extendedprice), 
+            avg_disc       = mean(l_discount), 
+            count_order    = n()) %>% 
         arrange(l_returnflag, l_linestatus)
 }
 
