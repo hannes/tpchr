cd65430c45e0b45c64b8539ba0be9caf2c204cc6
Hannes Muehleisen <hannes@muehleisen.org>
2018-01-30 17:02:48 +0100

    more cleanup

--- src/rstuff.c
+++ src/rstuff.c
@@ -1,6 +1,8 @@
 #include <Rdefines.h>
 #include <time.h>
 
+#define DECLARER				/* EXTERN references get defined here */
+
 #include "dss.h"
 #include "dsstypes.h"
 
@@ -10,9 +12,6 @@
 #define APPEND_INTSXP(val) INTEGER_POINTER(VECTOR_ELT(as, col++))[of] = (int) val;
 #define APPEND_NUMSXP(val) NUMERIC_POINTER(VECTOR_ELT(as, col++))[of] = (double) val;
 #define APPEND_STRSXP(val) SET_STRING_ELT( VECTOR_ELT(as, col++), of, RSTR(val));
-
-void gen_tbl(int tnum, DSS_HUGE start, DSS_HUGE count, long upd_num);
-void load_dists(void);
 
 static SEXP df_region = NULL;
 static size_t off_region = 0;
@@ -175,8 +174,6 @@ static int append_order(order_t *o, int mode) {
 	str[0] = o->orderstatus;
 	APPEND_STRSXP(str);
 	APPEND_NUMSXP((double)o->totalprice/100);
-	// TODO: use date objects for this?
-	//APPEND_STRSXP(o->odate);
 	APPEND_INTSXP(date_to_int(o->odate));
 	APPEND_STRSXP(o->opriority);
 	if (!lean) {
@@ -360,11 +357,133 @@ extern seed_t Seed[];
 seed_t Seed_backup[MAX_STREAM + 1];
 
 
+static int
+gen_tbl_R (int tnum, DSS_HUGE count)
+{
+	order_t o;
+	supplier_t supp;
+	customer_t cust;
+	part_t part;
+	code_t code;
+
+	for (DSS_HUGE i = 1; count; count--, i++) {
+		row_start(tnum);
+		switch (tnum)
+		{
+		case LINE:
+		case ORDER:
+  		case ORDER_LINE:
+			mk_order (i, &o, 0);
+			if (append_order_line(&o, 0) != 0) {
+				return -1;
+			}
+			break;
+		case SUPP:
+			mk_supp (i, &supp);
+			if (append_supp(&supp, 0) != 0) {
+				return -1;
+			}
+			break;
+		case CUST:
+			mk_cust (i, &cust);
+			if (append_cust(&cust, 0) != 0) {
+				return -1;
+			}
+			break;
+		case PSUPP:
+		case PART:
+  		case PART_PSUPP:
+			mk_part (i, &part);
+			if (append_part_psupp(&part, 0) != 0) {
+				return -1;
+			}
+			break;
+		case NATION:
+			mk_nation (i, &code);
+			if (append_nation(&code, 0) != 0) {
+				return -1;
+			}
+			break;
+		case REGION:
+			mk_region (i, &code);
+			if (append_region(&code, 0) != 0) {
+				return -1;
+			}
+			break;
+		}
+		row_stop(tnum);
+
+	}
+	return 0;
+}
+
+/*
+* read the distributions needed in the benchamrk
+*/
+static void
+load_dists (void)
+{
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "p_cntr", &p_cntr_set);
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "colors", &colors);
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "p_types", &p_types_set);
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "nations", &nations);
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "regions", &regions);
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "o_oprio",
+		&o_priority_set);
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "instruct",
+		&l_instruct_set);
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "smode", &l_smode_set);
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "category",
+		&l_category_set);
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "rflag", &l_rflag_set);
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "msegmnt", &c_mseg_set);
+
+	/* load the distributions that contain text generation */
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "nouns", &nouns);
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "verbs", &verbs);
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "adjectives", &adjectives);
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "adverbs", &adverbs);
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "auxillaries", &auxillaries);
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "terminators", &terminators);
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "articles", &articles);
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "prepositions", &prepositions);
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "grammar", &grammar);
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "np", &np);
+	read_dist (env_config (DIST_TAG, DIST_DFLT), "vp", &vp);
+
+}
+
+
+tdef tdefs[] =
+{
+	{"part.tbl", "part table", 200000,
+		NULL, NULL, PSUPP, 0},
+	{"partsupp.tbl", "partsupplier table", 200000,
+		NULL, NULL, NONE, 0},
+	{"supplier.tbl", "suppliers table", 10000,
+		NULL, NULL, NONE, 0},
+	{"customer.tbl", "customers table", 150000,
+		NULL, NULL, NONE, 0},
+	{"orders.tbl", "order table", 150000,
+		NULL, NULL, LINE, 0},
+	{"lineitem.tbl", "lineitem table", 150000,
+		NULL, NULL, NONE, 0},
+	{"orders.tbl", "orders/lineitem tables", 150000,
+		NULL, NULL, LINE, 0},
+	{"part.tbl", "part/partsupplier tables", 200000,
+		NULL, NULL, PSUPP, 0},
+	{"nation.tbl", "nation table", NATIONS_MAX,
+		NULL, NULL, NONE, 0},
+	{"region.tbl", "region table", NATIONS_MAX,
+		NULL, NULL, NONE, 0},
+};
+
+
+
 // 'inspired' by driver.c::main(). also, what a mess
 static SEXP dbgen_R(SEXP sf, SEXP leansxp) {
-	DSS_HUGE rowcnt = 0, minrow = 0;
+	DSS_HUGE rowcnt = 0;
 	double flt_scale;
-	long upd_num = 0;
 	DSS_HUGE i;
 	// all tables
 	table = (1 << CUST) | (1 << SUPP) | (1 << NATION) | (1 << REGION)
@@ -421,14 +540,6 @@ static SEXP dbgen_R(SEXP sf, SEXP leansxp) {
 	/* have to do this after init */
 	tdefs[NATION].base = nations.count;
 	tdefs[REGION].base = regions.count;
-
-	// setup loaders
-	tdefs[NATION].loader = append_nation;
-	tdefs[REGION].loader = append_region;
-	tdefs[CUST].loader = append_cust;
-	tdefs[SUPP].loader = append_supp;
-	tdefs[PART_PSUPP].loader = append_part_psupp;
-	tdefs[ORDER_LINE].loader = append_order_line;
 
 	off_cust = 0;
 	off_lineitem = 0;
@@ -559,13 +670,14 @@ static SEXP dbgen_R(SEXP sf, SEXP leansxp) {
 	 */
 	for (i = PART; i <= REGION; i++) {
 		if (table & (1 << i)) {
-			minrow = 1;
 			if (i < NATION)
 				rowcnt = tdefs[i].base * scale;
 			else
 				rowcnt = tdefs[i].base;
 			// actually doing something
-			gen_tbl((int) i, minrow, rowcnt, upd_num);
+			if (gen_tbl_R((int) i,  rowcnt) != 0) {
+				error("memory allocation");
+			}
 		}
 	}
 
@@ -597,7 +709,7 @@ static SEXP dbgen_R(SEXP sf, SEXP leansxp) {
 	SEXP names = PROTECT(NEW_CHARACTER(ntab));
 	if (!tables || !names) {
 		UNPROTECT(2);
-		error("memmory allocation");
+		error("memory allocation");
 		return NULL;
 	}
 	char* names_arr[] = { "region", "nation", "supplier", "customer", "part",
